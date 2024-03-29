a12a7b7e508e7ebd4b16523690e3a2dd40ee8b90
LG <lee3604mine4@naver.com>
2024-03-29 18:12:39 +0900

    usb 장치 인식 및 목록 출력

---
+++ USBprotect/InternalFunction/DevconClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// class information :: 
+// 프로그램 구현에 필요한 핵심 DEVCON 명령어들이 C# 에서 작동할 수 있도록 하는 함수형 명령코드의 집합입니다.
+// 필요한 명령어는 DevconCommand 의 매개변수로 제공해서 실행 함수를 따로 빼서 쓰시면 됩니다.
+
+namespace UsbSecurity
+{
+    class DevconClass
+    {
+        private string usbDeviceId; // USB 장치 
+        string devconPath = @"C:\Program Files (x86)\Windows Kits\10\Tools\10.0.22621.0\x64\devcon.exe"; // !! devcon 모듈의 경로에 대한 수정 요구됨 
+
+        private string DevconCommand(string command) // Devcon 명령어를 실행하는 메서드 , 매겨변수로 devcon 명령어를 받습니다. 
+        {
+            ProcessStartInfo psi = new ProcessStartInfo() // 프로세스 시작 정보
+            {
+                FileName = devconPath,         // Devcon 경로
+                Arguments = command,           // 명령어
+                UseShellExecute = false,       // 셸 실행 사용 안함
+                RedirectStandardOutput = true, // 표준 출력 리다이렉트
+                CreateNoWindow = true    // 창 생성 안함
+            };
+
+            using (Process process = Process.Start(psi)) // 프로세스 시작
+            {
+                using (StreamReader reader = process.StandardOutput) // 프로세스 읽어옴...
+                {
+                    string result = reader.ReadToEnd(); 
+                    return result;
+                }
+            }
+        }
+            
+        // --------------------------------------------- 요 아래에 함수 형태로 devcon 명령어 구현 --------------------------------------------------------- //
+
+        internal void DisableDevice() // 차단하기
+        {
+            DevconCommand("disable" + usbDeviceId);
+        }
+
+        internal void EnableDevice() // 허용하기 
+        {
+           DevconCommand("enable" + usbDeviceId);
+        }
+        
+    }
+}
---
+++ USBprotect/InternalFunction/ParsingUsbDevice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// class information :: 
+// 연결된 USB 저장장치 (이동저장장치만)의 인스턴스 ID 를 추출합니다. 
+// 차단 또는 승인할 장치의 인스턴스 아이디에 사용됩니다.
+
+
+
+namespace UsbSecurity
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Management; // System.Management 네임스페이스 참조 필요
+
+    class ParsingUsbDevice
+    {
+        private List<string> usbDeviceId = new List<string>(); // USB 장치 ID 가 저장되는 List 제너릭 
+
+        public static List<string> GetExternalDrivePNPDeviceIDs()
+        {
+            List<string> pnpDeviceIDs = new List<string>();
+
+            // 'Win32_DiskDrive'에서 이동식 드라이브만 필터링합니다.
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            // 'Win32_DiskDrive'의 PNPDeviceID를 가져옵니다.
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                var isRemovable = queryObj["MediaType"] != null && queryObj["MediaType"].ToString().Contains("Removable");
+
+                // 이동식 미디어를 가진 디스크 드라이브의 PNPDeviceID를 추출합니다.
+                if (isRemovable)
+                {
+                    // PNPDeviceID 속성 값을 리스트에 추가
+                    pnpDeviceIDs.Add(queryObj["PNPDeviceID"].ToString());
+                }
+            }
+            return pnpDeviceIDs;
+        }
+
+        public void InsertData()
+        {
+            var externalDriveIDs = GetExternalDrivePNPDeviceIDs(); // 이동식 드라이브 인스턴스 ID를 가져옴 자료형은 
+            foreach (var id in externalDriveIDs) // 왜 VAR 형인가 ? 
+            {
+                usbDeviceId.Add(id); //usb 장치 목록에 추출된 아이디를 추가한다.
+            }
+
+        }
+
+        public void ShowList()
+        { // usb 장치의 인스턴스 id 를 출력하는 함수
+            Console.WriteLine("USB 장치 목록");
+            foreach (var number in usbDeviceId)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+
+
+}
