a3a18232be26345b193162aa0b201766dc578ce5
4xvgal <4xvgal@protonmail.com>
2024-05-16 12:58:28 +0900

    merging lgj

---
+++ USBprotect/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace USBprotect
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.종료 = new System.Windows.Forms.ToolStripMenuItem();
+            this.최대화 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbg = new System.Windows.Forms.Button();
+            this.tbgT = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Location = new System.Drawing.Point(402, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(413, 212);
+            this.panel2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(199)))), ((int)(((byte)(118)))));
+            this.label4.Location = new System.Drawing.Point(304, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "내보내기";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(199)))), ((int)(((byte)(118)))));
+            this.label3.Location = new System.Drawing.Point(145, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "허용요청관리";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(199)))), ((int)(((byte)(118)))));
+            this.label2.Location = new System.Drawing.Point(10, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "장치목록관리";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(199)))), ((int)(((byte)(118)))));
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Location = new System.Drawing.Point(-37, 92);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(838, 508);
+            this.panel3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(343, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(3, 238);
+            this.label6.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Location = new System.Drawing.Point(74, 72);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(211, 212);
+            this.panel4.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(199)))), ((int)(((byte)(118)))));
+            this.label5.Location = new System.Drawing.Point(47, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "허용요청보내기";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(219)))), ((int)(((byte)(45)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tbg);
+            this.panel1.Controls.Add(this.tbgT);
+            this.panel1.Location = new System.Drawing.Point(-19, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(364, 64);
+            this.panel1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("HY견고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(96, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 18);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "장치 모니터링 정상 작동중";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.종료,
+            this.최대화});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(133, 56);
+            // 
+            // 종료
+            // 
+            this.종료.Name = "종료";
+            this.종료.Size = new System.Drawing.Size(132, 26);
+            this.종료.Text = "종료";
+            this.종료.Click += new System.EventHandler(this.종료_Click);
+            // 
+            // 최대화
+            // 
+            this.최대화.Name = "최대화";
+            this.최대화.Size = new System.Drawing.Size(132, 26);
+            this.최대화.Text = "최대화";
+            this.최대화.Click += new System.EventHandler(this.최대화_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(219)))), ((int)(((byte)(45)))));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::USBprotect.Properties.Resources.off_1;
+            this.button4.Location = new System.Drawing.Point(648, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 55);
+            this.button4.TabIndex = 14;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(186)))), ((int)(((byte)(146)))));
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Image = global::USBprotect.Properties.Resources.Send;
+            this.button5.Location = new System.Drawing.Point(43, 21);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 120);
+            this.button5.TabIndex = 1;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(186)))), ((int)(((byte)(146)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::USBprotect.Properties.Resources.Numbered_List;
+            this.button1.Location = new System.Drawing.Point(13, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 120);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(186)))), ((int)(((byte)(146)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::USBprotect.Properties.Resources.Circled_Check;
+            this.button2.Location = new System.Drawing.Point(148, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 120);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(186)))), ((int)(((byte)(146)))));
+            this.button3.BackgroundImage = global::USBprotect.Properties.Resources.Move_Mail;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(283, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 120);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbg
+            // 
+            this.tbg.BackColor = System.Drawing.Color.Transparent;
+            this.tbg.BackgroundImage = global::USBprotect.Properties.Resources.shield;
+            this.tbg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tbg.Location = new System.Drawing.Point(20, 4);
+            this.tbg.Name = "tbg";
+            this.tbg.Size = new System.Drawing.Size(55, 49);
+            this.tbg.TabIndex = 3;
+            this.tbg.UseVisualStyleBackColor = false;
+            this.tbg.Click += new System.EventHandler(this.tbg_Click);
+            // 
+            // tbgT
+            // 
+            this.tbgT.BackColor = System.Drawing.Color.Transparent;
+            this.tbgT.BackgroundImage = global::USBprotect.Properties.Resources.disableshield;
+            this.tbgT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.tbgT.Location = new System.Drawing.Point(20, -5);
+            this.tbgT.Name = "tbgT";
+            this.tbgT.Size = new System.Drawing.Size(55, 69);
+            this.tbgT.TabIndex = 14;
+            this.tbgT.UseVisualStyleBackColor = false;
+            this.tbgT.Click += new System.EventHandler(this.tbgT_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button tbg;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 종료;
+        private System.Windows.Forms.ToolStripMenuItem 최대화;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button tbgT;
+    }
+}
--- USBprotect/ManageMent/ManageAllowList.cs
+++ USBprotect/ManageMent/ManageAllowList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -9,9 +10,12 @@ using UsbSecurity;
 
 namespace USBsecurity
 {
+
     class ManageAllowList
     {
         DevconCMD devconCMD = new DevconCMD(); // devconCMD 클래스 인스턴스 생성 
+
+
 
         public ManageAllowList()
         {
@@ -20,6 +24,7 @@ namespace USBsecurity
         }
 
         // blackList 컬렉션에 변경이 발생할 때 호출되는 메서드
+
         private void WhitekList_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             // 변경 유형이 추가일 경우 실행 (ㄹㅇ 이게 뭐냐면 새로운 항목이 추가되었을 때 실행)
@@ -29,20 +34,38 @@ namespace USBsecurity
                 foreach (var Item in e.NewItems) // 새로 추가된 항목들을 가져옴
                 {
                     USBinfo newItem = Item as USBinfo; // USBinfo 클래스로 캐스팅
-                    disableEveryDevice(newItem.DeviceId); // 장치 활성화 해버리기
+ 
+                    enableEveryDevice(newItem.DeviceId); // 장치 활성화 해버리기
                 }
             }
         }
-        public void disableEveryDevice(string DeviceId) // 장치 활성화 메서드
+        public void enableEveryDevice(string DeviceId) // 장치 활성화 메서드
         {
-            devconCMD.DevconCommand($"enable \"{DeviceId}\""); //명령어 실행   
+            devconCMD.DevconCommand($"enable \"@{DeviceId}\""); //명령어 실행   
         }
 
-        public void BlacktoWhite(string deviceid)
+        public void BlackToWhite(string deviceid)
         {
-            
-            USBinfo.WhiteListDevices.Add(USBinfo.BlackListDevices.FirstOrDefault(x => x.PnpDeviceId.Trim().Equals(deviceid.Trim(), StringComparison.OrdinalIgnoreCase)));
+            var device = USBinfo.BlackListDevices.FirstOrDefault(x => x.PnpDeviceId.Trim().Equals(deviceid.Trim(), StringComparison.OrdinalIgnoreCase));
 
+            if (device != null)
+            {   
+                // 비동기 처리: 장치를 화이트리스트로 이동
+                // 임시 변수를 사용하여 컬렉션 변경 이벤트 핸들러가 완료된 후 컬렉션 수정
+                var toAdd = device;
+                var toRemove = device;
+
+                // 실제 컬렉션 수정은 이벤트 핸들러 외부에서 수행
+                Task.Run(() => {
+                    USBinfo.WhiteListDevices.Add(toAdd);
+                    USBinfo.BlackListDevices.Remove(toRemove);
+                });
+            }
+            else
+            {
+                // 예외 처리: 객체가 존재하지 않음
+                MessageBox.Show($"Device with ID {deviceid} not found in whitelist.");
+            }
         }
 
     }
